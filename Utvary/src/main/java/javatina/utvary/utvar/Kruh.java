781738b94de2b1c1830a673034342c5064f51a91
TinaJava <cativa@azet.sk>
2019-11-28 16:39:02 +0100

    vytvorene triedy

---
+++ Utvary/src/main/java/javatina/utvary/utvar/Kruh.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatina.utvary.utvar;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author vlada
+ */
+public class Kruh extends Utvar {
+
+    @Override
+    public void vykresliSa(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+   
+    }
+
+   
+
+   
+
+  
+    
+
+    
+
