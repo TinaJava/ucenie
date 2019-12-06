04f2856b80cba71a8c9067ac0208ac5852ebd34a
TinaJava <cativa@azet.sk>
2019-12-06 12:41:24 +0100

    urobila som  list aj pole zvierat a opakovane mi nevypisuje ich hodnoty tam budem mat problem dakde v tej} ale neviem kde

--- Farma/src/main/java/javatina/farma/Starter.java
+++ Farma/src/main/java/javatina/farma/Starter.java
@@ -5,6 +5,8 @@
  */
 package javatina.farma;
 
+import java.util.ArrayList;
+import java.util.List;
 import javatina.farma.zviera.Kivi;
 import javatina.farma.zviera.Macka;
 import javatina.farma.zviera.Pes;
@@ -34,6 +36,30 @@ public class Starter {
         macka = new Macka("Luigi", 25.8, 4.5, true, true, true, false);
         System.out.println(macka);
 
-    }
+        // vytvorenie pola zvierat a jeho naplnenie
+        Zviera[] zvierata;
+        zvierata = new Zviera[3];
+        zvierata[0] = new Kivi("kiva", 5.2, 0.55, true, true, true);
+        zvierata[1] = new Pes("Mort", 110.2, 95.5, true, true, true);
+        zvierata[2] = new Macka("Luigi", 35.2, 7.2, true, true, true, false);
 
+        for (Zviera zvieratko : zvierata) {
+            System.out.println(zvieratko);
+
+            //vytvorenie listu zvierat a jeho naplnenie
+            List<Zviera> zvieratka;
+            zvieratka = new ArrayList<>();
+            zvieratka.add(new Kivi("skrecok", 5.2, 0.55, true, true, true));
+            zvieratka.add(new Pes("velky strazny pes", 110.2, 105.5, true, true, true));
+            zvieratka.add(new Macka("ciciak", 35.2, 6.8, true, true, true, true));
+
+            System.out.println("....................................................");
+            System.out.println("vypis domacich zvierat s ich vlastnostami");
+
+            for (Zviera zver : zvieratka) {
+                System.out.println(zver);
+
+            }
+        }
+    }
 }
