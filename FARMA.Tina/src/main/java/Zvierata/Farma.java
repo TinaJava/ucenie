7f763ea786d2dcd14bf1d5d6c3d1148e99acd51b
mstevlik@gmail.com <mstevlik@gmail.com>
2020-01-02 14:26:04 +0100

    - takto som to chcel, uz to len dokonci

--- FARMA.Tina/src/main/java/Zvierata/Farma.java
+++ FARMA.Tina/src/main/java/Zvierata/Farma.java
@@ -5,7 +5,6 @@
  */
 package Zvierata;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -16,11 +15,23 @@ public class Farma {
 
     private List<Zviera> zvierata;
 
-    public Farma(List<Zviera> zvierata, int pocetUzitkove) {
-        this.zvierata = zvierata;
-       
-            
+    public Farma() {  // konstruktor som chcel bez parametrov
+        this.zvierata = // tuto nieco bude zacinajuce na new ...;
     }
+    
+    /**
+     * Metoda vypise zoznam zvierat na farme
+     */
+    public void vypisZvierat() {
+        // tu sprav vypis zvierat z listu "zvierata" (pomocou for a sout)
+    }
+    
+    /**
+     * Metoda prida do zoznamu zvierat na farme dalsie zviera
+     * @param zviera - zviera, kt. sa ma pridat
+     */
+    public void pridajZviera(Zviera zviera) {
+        // tu urob pridanie zviarata zo vstupneho parametra do listu "zvierata"
+    }
+
 }
-
-
